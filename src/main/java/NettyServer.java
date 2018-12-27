0a12c31460c20251328cb37e9b25ac41ba92fb9b
zbl935754045 <935754045@qq.com>
2018-12-27 15:08:44 +0800

    '客户端v1.1'

--- src/main/java/NettyServer.java
+++ src/main/java/NettyServer.java
@@ -1,5 +1,23 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class NettyServer {
     public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) {
 
+                    }
+                });
+        serverBootstrap.bind(8000);
     }
 }
