7488b9ce98b43bd3c2c36ad58b2739b84ccdc2e8
zbl935754045 <935754045@qq.com>
2018-12-27 18:06:48 +0800

    服务端递增绑定端口

--- src/main/java/NettyServer.java
+++ src/main/java/NettyServer.java
@@ -1,23 +1,46 @@
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
 
 public class NettyServer {
-    public static void main(String[] args) {
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-        ServerBootstrap serverBootstrap = new ServerBootstrap();
-        serverBootstrap
-                .group(bossGroup,workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override
-                    protected void initChannel(NioSocketChannel nioSocketChannel) {
+    private static final int BEGIN_PORT = 8000;
 
+    public static void main(String[] args) {
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+        final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
+        serverBootstrap
+                .group(boosGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                .childAttr(clientKey, "clientValue")
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        System.out.println(ch.attr(clientKey).get());
                     }
                 });
-        serverBootstrap.bind(8000);
+
+
+        bind(serverBootstrap, BEGIN_PORT);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+                bind(serverBootstrap, port + 1);
+            }
+        });
     }
 }
