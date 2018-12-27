b0508da0cffe6a211c5fd2fea174cd620462fbd2
zbl935754045 <935754045@qq.com>
2018-12-27 23:03:25 +0800

    客户端启动流程

--- src/main/java/NettyClient.java
+++ src/main/java/NettyClient.java
@@ -1,2 +1,51 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class NettyClient {
+    private static final int MAX_RETRY=5;
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                .group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .attr(AttributeKey.newInstance("clientName"),"nettyClientg")
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,5000)
+                .option(ChannelOption.SO_KEEPALIVE,true)
+                .option(ChannelOption.TCP_NODELAY,true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch){
+                    }
+                });
+        connect(bootstrap,"127.0.0.1",8000,MAX_RETRY);
+    }
+
+    private static void connect(Bootstrap bootstrap,String host,int port,int retry){
+        bootstrap.connect(host,port).addListener(future -> {
+           if (future.isSuccess()){
+               System.out.println("连接成功");
+           }else if(retry==0) {
+               System.out.println("重试次数已用完，放弃连接");
+           }else {
+               int order = (MAX_RETRY-retry)+1;
+               int delay = 1<<order;
+               System.err.println(new Date()+"连接失败，第"+order+"次连接...");
+               bootstrap.config().group().schedule(()->connect(bootstrap,host,port,retry-1),delay,TimeUnit.SECONDS);
+
+           }
+        });
+    }
+
+
+
 }
+
--- src/main/java/NettyServer.java
+++ src/main/java/NettyServer.java
@@ -18,11 +18,19 @@ public class NettyServer {
         serverBootstrap
                 .group(boosGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
+                //attr()方法可以给服务端的 channel，也就是NioServerSocketChannel指定一些自定义属性
+                //我们可以通过channel.attr()取出这个属性
+                //通常情况下，我们也用不上这个方法
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                //childAttr可以给每一条连接指定自定义属性
+                //然后后续我们可以通过channel.attr()取出该属性。
                 .childAttr(clientKey, "clientValue")
+                //给服务端channel设置一些属性
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                //childOption()可以给每条连接设置一些TCP底层相关的属性
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                //childHandler()用于指定处理新连接数据的读写处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
